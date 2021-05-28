5fe5a70d253212018ba2bd5875f67e8355fa93d2
zal-byte <rizal.solehudin45@gmail.com>
2021-05-28 08:59:42 +0700

    make it better again

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -38,7 +38,27 @@
 		</nav>
 	</header>
 	<section>
-		
+		<div class="container p-4">
+			<div class="row">
+				<div class="col">
+					<div class="card border-0 shadow">
+						<div class="card-header border-0 bg-light">
+							<h4> My First Project </h4>
+						</div>
+						<div class="card-body">
+							<p>
+								 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 </body>
 </html>
